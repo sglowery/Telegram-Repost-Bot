ff9e6c3e6324a9d621adbbcfb0195cc8d097becd
sglowery <stephen.g.lowery@gmail.com>
2019-02-23 12:51:46 -0700

    Initial commit

---
+++ config_example.py
@@ -0,0 +1,6 @@
+REPOST_BOT_TOKEN = "Your Telegram bot token here"
+REPOST_ALERT_STRING = "!!! REPOST ALERT! REPOST ALERT !!!"
+PRIVATE_CHAT_STRING = "I only check for reposts in groups!"
+HELP_STRING = "I'm {name}. I analyze the pictures you send and call out reposts. Only a hash of the image and the id" \
+              "of the message that sent a picture is stored."
+GROUP_REPOST_PATH = "groups/"
