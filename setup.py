14e83e85c728625220a15dd432326dc8961b881f
sglowery <stephen.g.lowery@gmail.com>
2019-02-23 13:15:34 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='reeepostbot',
+    version='0.1',
+    packages=['imagehash', 'python-telegram-bot'],
+    url='',
+    license='GNU General Public License v3.0',
+    author='Stephen',
+    author_email='stephen.g.lowery@gmail.com',
+    description='A Telegram bot to track images sent in a group chat and call out reposts.'
+)
