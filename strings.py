835900587eac5b44a94651ff8b0075df648110c5
sglowery <stephen.g.lowery@gmail.com>
2019-10-29 21:59:59 -0600

    added ability to reset a group's repost data, increased the precision of the hash used for pictures, started hashing URLs, added a new master bot admin ID and split the strings from the tokens and IDs

--- strings.py
+++ strings.py
@@ -3,11 +3,15 @@ PRIVATE_CHAT_STRING = "I don't care about reposts just between us. I only check 
                       "dingus"
 PRIVATE_CHAT_TOGGLE_STRING = "I don't track reposts in private chats."
 HELP_STRING = "I'm {name}. I analyze the pictures and URLs you send and call out reposts. I store the message id, " \
-              "URLs in plaintext and a hash of the image to accomplish this."
+              "and hashed versions of the URLs and pictures you send. GIFs and videos are not tracked."
 REPOST_NOTIFIERS = ["FIRST POSTED HERE", "AND THEN HERE",
                     "REEEEEE ORIGINAL CONTENT PLEASE {name}!!!!!!!!!!!!!!!!! AAAAAAAAAAAA"]
 INVALID_WHITELIST_REPLY = "Reply to a message containing a picture or URL you want whitelisted."
 ALREADY_WHITELISTED_REPLY = "That's already whitelisted!"
 SUCCESSFUL_WHITELIST_REPLY = "I won't track reposts of that from now on."
+GROUP_REPOST_RESET_INITIAL_PROMPT = "Are you sure you want to delete the group's repost data? This is irreversible."
+GROUP_REPOST_RESET_ADMIN_ONLY = "Only admins can do this action."
+GROUP_REPOST_RESET_NO = "I won't delete it then."
+GROUP_REPOST_DATA_RESET = "The group repost data has been reset."
 GROUP_REPOST_PATH = "groups/"
-DEFAULT_CALLOUT = {"url": False, "picture": True}
+DEFAULT_CALLOUT = {"url": True, "picture": True}
