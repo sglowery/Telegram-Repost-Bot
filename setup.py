aaa37ae8cb5a48b9df1d0317f445052054f5514d
sglowery <stephen.g.lowery@gmail.com>
2019-11-14 21:46:38 -0700

    fix setup.py installation requirements

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='reeepostbot',
     version='0.5',
     url='',
-    modules=['repostbot', 'config'],
+    modules=['repostbot'],
     license='GNU General Public License v3.0',
     author='Stephen Lowery',
     author_email='stephen.g.lowery@gmail.com',
     description='A Telegram bot to track images sent in a group chat and call out reposts.',
-    install_requires=['imagehash', 'python-telegram-bot', 'telegram', 'Pillow']
+    install_requires=['imagehash', 'python-telegram-bot', 'telegram', 'Pillow', 'pyyaml']
 )
