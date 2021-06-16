4675086098f3fb05dec7733d0db5516988c6579a
sglowery <stephen.g.lowery@gmail.com>
2021-06-16 17:58:56 -0600

    bump Pillow and python-telegram-bot versions and simplify requirements.txt

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='reeepostbot',
-    version='0.2.1',
+    version='0.2.2',
     url='https://github.com/sglowery/Telegram-Repost-Bot',
     modules=['repostbot'],
     license='GNU General Public License v3.0',
     author='Stephen Lowery',
     author_email='stephen.g.lowery@gmail.com',
     description='A Telegram bot to track images sent in a group chat and call out reposts.',
-    install_requires=['imagehash', 'python-telegram-bot', 'telegram', 'Pillow', 'pyyaml']
+    install_requires=['imagehash', 'python-telegram-bot', 'telegram', 'pillow', 'pyyaml']
 )
