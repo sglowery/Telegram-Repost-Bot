ad19122f78c1bf2095304b7442067873a725bce6
sglowery <stephen.g.lowery@gmail.com>
2022-09-13 22:25:08 -0600

    fix version number and add missing dependency in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='telegram-repost-bot',
-    version='0.4.0',
+    version='0.5.0',
     url='https://github.com/sglowery/Telegram-Repost-Bot',
     modules=['repostbot'],
     license='GNU General Public License v3.0',
     author='Stephen Lowery',
     author_email='stephen.g.lowery@gmail.com',
     description='A Telegram bot to track images sent in a group chat and call out reposts.',
-    install_requires=['imagehash', 'python-telegram-bot', 'telegram', 'pillow', 'pyyaml', 'python-dotenv']
+    install_requires=['imagehash', 'python-telegram-bot', 'telegram', 'pillow', 'pyyaml', 'python-dotenv', 'ujson'],
 )
